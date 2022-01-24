ce0cd4d37b138752e82e1ff9a8dd286c791b8ebe
Raghu Devulaapalli <RDevulaapalli@hudson-advisors.com>
2022-01-24 09:23:44 +0530

    Add project files.

---
+++ ConsoleApp3/Deposits.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    public class Deposit:BankAccount
+    {
+        //Properties
+        public decimal DepositAmount
+        {
+            get;set;
+        }
+
+        public String DepositedBy
+        {
+            get; set;
+        }
+
+        public DateTime DepositedOn
+        {
+            get; set;
+        }
+
+        public bool DoDeposit()
+        {
+            return true;
+        }
+        
+    }
+
+    public class Withdraw:BankAccount
+    {
+        //Properties
+        public decimal WithdrawAmount
+        {
+            get; set;
+        }
+
+        public String WithdrawnBy
+        {
+            get; set;
+        }
+
+        public DateTime WithdrawnOn
+        {
+            get; set;
+        }
+
+        public bool DoWithdraw()
+        {
+            return true;
+        }
+    }
+
+    public class BankAccount
+    {
+        //Properties
+        public decimal Amount
+        {
+            get; set;
+        }        
+
+        public String BranchId
+        {
+            get; set;
+        }
+
+        public string BankAccountId { get; set; }
+        public string BankAccountName { get; set; }
+       
+        public double GetAccountBalance(string bankAccountId)
+        {
+            return 100;
+        }
+
+        public virtual double SetAccountBalance(string bankAccountId, double amount, string action = "Deposit")
+        {
+            double bankBalance = 10000;
+            if (action == "Deposit")
+                bankBalance += amount;
+            else if (action == "Withdraw")
+                bankBalance -= amount;
+            return bankBalance;
+        }
+
+    }
+
+    public class BankAdvancedOperations : BankAccount
+    {
+        public override double SetAccountBalance(string bankAccountId, double amount, string action = "Deposit")
+        {
+            //Do some advanced actions
+            return 100;
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Withdraw withdraw = new Withdraw();
+            withdraw.SetAccountBalance(withdraw.BankAccountId, 900, action: "Withdraw");
+        }
+    }
+}
